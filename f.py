47c830d5dcffdc87690ed7911092d5cc7cbe5874
Naman Singla <75626821+nsingla20@users.noreply.github.com>
2021-05-19 01:54:33 +0530

    Uploaded Code

---
+++ f.py
@@ -0,0 +1,19 @@
+import time
+import requests
+import csv
+import json
+def browse_and_scrape(url):
+    with open("gsoc2021.csv",'w') as file:
+        writer=csv.writer(file)
+        writer.writerow(["NAME","Organisation","Project"])
+        try:
+            while(url!=None):
+                response=json.loads(requests.get(url).text)
+                for i in range(40):
+                    writer.writerow([response['results'][i]['student']['display_name'],response['results'][i]['organization']['name'],response['results'][i]['title']])
+                url=response['next']
+        except:
+            print("Index exceeded. Exiting...")
+        finally:
+            print("COMPLETED!")
+browse_and_scrape("https://summerofcode.withgoogle.com/api/program/current/project/?page=1&page_size=40")
